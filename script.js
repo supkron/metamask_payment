ea8c813311a50c8a8fe51f8f8122614ab83677f3
DIGITSOUL\suphakorn.b <suphakorn.b@digitsoul.co.th>
2023-01-30 11:18:15 +0700

    nft tranfer

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ d.querySelector("#donate_btn").addEventListener("click",()=>{
         method:"eth_sendTransaction",
         params: [{
             from:ethereum.selectedAddress,
-            to:"0xFc102e94230788513F71579B4B863d8fF89BeB25",
+            to:"0xAd8619Ef0422464cdA1d85826613faCCE6a71d7b",
             value:convertToWei(Number(price))// 1eth = 10^18 wei
         }]
     })
     .then((txtHash)=>{
-        setAlert("ขอบคุณที่สนับสนุนเรา! (txtHash :"+txtHash+" )","success");
+        setAlert("ดำเนินการเรียบร้อย! (txtHash :"+txtHash+" )","success");
     })
     .catch((err)=>{
         console.log(err);
@@ -37,6 +37,15 @@ d.querySelector("#donate_btn").addEventListener("click",()=>{
         }
     })
 });
+ethereum.request({
+    method: "eth_getBalance",
+    params: [ethereum.selectedAddress, "latest"]
+}).then(balance => {
+    let etherBalance = web3.utils.fromWei(balance, "ether");
+    console.log("Ether Balance: ", etherBalance);
+    d.querySelector("#balance").innerHTML = etherBalance + " ETH";
+});
+
 
 function convertToWei(price){
     return "0x"+Number(price*1e+18).toString(16);
@@ -48,3 +57,27 @@ function setAlert(txt,alertColor){
     alertBox.innerHTML = txt;
     alertBox.className = "alert alert-"+alertColor;
 }
+ethereum.request({
+    method: "eth_getBalance",
+    params: [ethereum.selectedAddress, "latest"]
+}).then(balance => {
+    // Convert the balance from Wei to Ether
+    let etherBalance = web3.utils.fromWei(balance, "ether");
+    // Display the balance on the page
+    let balanceElement = document.querySelector("#balance");
+    balanceElement.innerHTML = etherBalance + " ETH";
+});
+// JavaScript code to retrieve the user's balance and update the HTML element
+// JavaScript code to retrieve the user's balance and update the HTML element
+ethereum.request({
+    method: "eth_getBalance",
+    params: [ethereum.selectedAddress, "latest"]
+}).then(balance => {
+    // Convert the balance from Wei to Ether
+    let etherBalance = web3.utils.fromWei(balance, "ether");
+    // Select the HTML element where you want to display the balance
+    let balanceElement = document.querySelector("#balance");
+    // Update the innerHTML of the element with the balance
+    balanceElement.innerHTML = etherBalance + " ETH";
+});
+
